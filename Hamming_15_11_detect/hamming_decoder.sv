1c7bdda82a8c92d4f51ac77edcad9a667c1b3aa4
zeeeli <zee578995@gmail.com>
2025-06-25 17:22:29 -0700

    Corrected bit packing order

--- Hamming_15_11_detect/hamming_decoder.sv
+++ Hamming_15_11_detect/hamming_decoder.sv
@@ -12,26 +12,27 @@ module hamming_decoder (
   logic [ 4:0] s;  // Syndrome
   logic [ 4:0] sed_location;  // Syndrome
   logic        s_nonzero;  // Neccesary to get around iVerilog quirk
+
   // NOTE: Bits being packed from MSB - LSB (like encoder)
   assign d = {
-    codeword_in[3],  // d0  (position 3)
-    codeword_in[5],  // d1  (position 5)
-    codeword_in[6],  // d2  (position 6)
-    codeword_in[7],  // d3  (position 7)
-    codeword_in[9],  // d4  (position 9)
-    codeword_in[10],  // d5  (position 10)
-    codeword_in[11],  // d6  (position 11)
-    codeword_in[12],  // d7  (position 12)
-    codeword_in[13],  // d8  (position 13)
-    codeword_in[14],  // d9  (position 14)
-    codeword_in[15]  // d10 (position 15)
+    codeword_in[15],  // d10 → d[10]
+    codeword_in[14],  // d9
+    codeword_in[13],  // d8
+    codeword_in[12],  // d7
+    codeword_in[11],  // d6
+    codeword_in[10],  // d5
+    codeword_in[9],  // d4
+    codeword_in[7],  // d3
+    codeword_in[6],  // d2
+    codeword_in[5],  // d1
+    codeword_in[3]  // d0 → d[0]
   };
 
-  assign p = {codeword_in[0], codeword_in[1], codeword_in[2], codeword_in[4], codeword_in[8]};
+  assign p = {codeword_in[8], codeword_in[4], codeword_in[2], codeword_in[1], codeword_in[0]};
 
   // ---------- Computing Syndrome bits
   // repeating parity check from encoder
-  assign check[0] = ^codeword_in;
+  assign check[0] = ^codeword_in[15:1];
   assign check[1] = d[0] ^ d[1] ^ d[3] ^ d[4] ^ d[6] ^ d[8] ^ d[10];
   assign check[2] = d[0] ^ d[2] ^ d[3] ^ d[5] ^ d[6] ^ d[9] ^ d[10];
   assign check[3] = d[1] ^ d[2] ^ d[3] ^ d[7] ^ d[8] ^ d[9] ^ d[10];
@@ -44,7 +45,6 @@ module hamming_decoder (
   assign s[3] = p[3] ^ check[3];
   assign s[4] = p[4] ^ check[4];
 
-  // Interpreting syndrome
   assign s_nonzero = |s[4:1];
   assign sed = s_nonzero & s[0];
   assign ded = s_nonzero & ~s[0];
@@ -54,7 +54,7 @@ module hamming_decoder (
   always_comb begin : sec
     codeword_corr = codeword_in;
     if (sed && sed_location != 0) begin
-      codeword_corr[sed_location-1] = ~codeword_in[sed_location-1];
+      codeword_corr[sed_location] = ~codeword_in[sed_location];
     end
   end
 
